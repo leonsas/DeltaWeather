65784b32510ef52c44275935f5ca5e8e6b71bbc2
Leon Sasson <leonsassonha@gmail.com>
2013-04-09 11:18:13 -0500

    formatted code, removed old code

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,12 +14,11 @@ $(function() {
 			//use HTML5 geolocation API with our callback to show the position
 			navigator.geolocation.getCurrentPosition(getConditions);
 		} else {
-			console.log("geolocation not supported");
+			alert("geolocation not supported");
 		}
 	}
 
 	function getConditions(position) {
-		//document.body.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
 		getCityLocation(position.coords.latitude, position.coords.longitude);
 	}
 
@@ -36,14 +35,14 @@ $(function() {
 				city = data.location.city;
 
 				//Lets get the current conditions for this state and city.
-				getCurrentConditions(state,city);
-				getWeather(state,city);
+				getCurrentConditions(state, city);
+				getWeather(state, city);
 			}
 		});
 
 	};
 
-	function getCurrentConditions(state,city){
+	function getCurrentConditions(state, city) {
 
 		//new URL
 		conditionURL = baseURL + '/conditions/q/' + state + '/' + city + '.json';
@@ -56,95 +55,92 @@ $(function() {
 			dataType : "jsonp",
 			success : function(data) {
 
-			$.ajax({
-			        url : conditionURL,
-			        dataType : "jsonp",
-			        success : function(data2) {
-				current = data2.current_observation.temp_f;
-				yesterday = data.history.dailysummary[0].meantempi;
-				test = current - yesterday;
-				test = test.toFixed(0);
-				if (test > 0)
-				{
-				stuff.innerHTML = 'It is ' + test + ' degrees warmer than yesterday.';
-				}
-				else
-				{
-				test *= -1;
-				stuff.innerHTML = 'It is ' + test + ' degrees colder than yesterday.';
-				}}
-			      });
-						 }
-		     });
-};
+				$.ajax({
+					url : conditionURL,
+					dataType : "jsonp",
+					success : function(data2) {
+						current = data2.current_observation.temp_f;
+						yesterday = data.history.dailysummary[0].meantempi;
+						test = current - yesterday;
+						test = test.toFixed(0);
+						if (test > 0) {
+							stuff.innerHTML = 'It is ' + test + ' degrees warmer than yesterday.';
+						} else {
+							test *= -1;
+							stuff.innerHTML = 'It is ' + test + ' degrees colder than yesterday.';
+						}
+					}
+				});
+			}
+		});
+	};
 
-	function getWeather(state,city){
-	weatherURL = baseURL + '/conditions/q/' + state + '/' + city + '.json';
-	var weatherpic = document.getElementById('weatherpic');
-	$.ajax({
-		url : weatherURL,
-		dataType : "jsonp",
-		success : function(data) {
-		changeIcon(data.current_observation.weather);
-	}
-	});	
-};
+	function getWeather(state, city) {
+		weatherURL = baseURL + '/conditions/q/' + state + '/' + city + '.json';
+		var weatherpic = document.getElementById('weatherpic');
+		$.ajax({
+			url : weatherURL,
+			dataType : "jsonp",
+			success : function(data) {
+				changeIcon(data.current_observation.weather);
+			}
+		});
+	};
 
-	function changeIcon(conditions){
-	var icons = new Skycons();
-	switch(conditions)
-	{
-	case "Clear":
-	icons.set("condition", Skycons.CLEAR_DAY);
-	break;
-	case "Cloudy":
-	icons.set("condition", Skycons.CLOUDY);
-	break;
-	case "Rain":
-	icons.set("condition", Skycons.RAIN);
-	break;
-	case "Sleet":
-	icons.set("condition", Skycons.SLEET);
-	break;
-	case "Snow":
-	icons.set("condition", Skycons.SNOW);
-	break;
-	case "Fog":
-	icons.set("condition", Skycons.FOG);
-	break;
-	case "Thunderstorms":
-	icons.set("condition", Skycons.RAIN);
-	break;
-	case "Thunderstorm":
-	icons.set("condition", Skycons.RAIN);
-	break;
-	case "Overcast":
-	icons.set("condition", Skycons.CLOUDY);
-	break;
-	case "Scattered Clouds":
-	icons.set("condition", Skycons.CLOUDY);
-	break;
-	case "Mostly Cloudy":
-	icons.set("condition", Skycons.CLOUDY);
-	break;
-	case "Mostly Sunny":
-	icons.set("condition", Skycons.CLEAR_DAY);
-	break;
-	case "Partly Cloudy":
-	icons.set("condition", Skycons.PARTLY_CLOUDY_DAY);
-	break;
-	case "Partly Sunny":
-	icons.set("condition", Skycons.PARTLY_CLOUDY_DAY);
-	break;
-	case "Freezing Rain":
-	icons.set("condition", Skycons.SLEET);
-	break;
-	default:
-	icons.set("condition", Skycons.CLEAR_DAY);
-	break;
-	}
-	icons.play();
+	function changeIcon(conditions) {
+		var icons = new Skycons();
+		switch(conditions) {
+			case "Clear":
+				icons.set("condition", Skycons.CLEAR_DAY);
+				break;
+			case "Cloudy":
+				icons.set("condition", Skycons.CLOUDY);
+				break;
+			case "Rain":
+				icons.set("condition", Skycons.RAIN);
+				break;
+			case "Sleet":
+				icons.set("condition", Skycons.SLEET);
+				break;
+			case "Snow":
+				icons.set("condition", Skycons.SNOW);
+				break;
+			case "Fog":
+				icons.set("condition", Skycons.FOG);
+				break;
+			case "Thunderstorms":
+				icons.set("condition", Skycons.RAIN);
+				break;
+			case "Thunderstorm":
+				icons.set("condition", Skycons.RAIN);
+				break;
+			case "Overcast":
+				icons.set("condition", Skycons.CLOUDY);
+				break;
+			case "Scattered Clouds":
+				icons.set("condition", Skycons.CLOUDY);
+				break;
+			case "Mostly Cloudy":
+				icons.set("condition", Skycons.CLOUDY);
+				break;
+			case "Mostly Sunny":
+				icons.set("condition", Skycons.CLEAR_DAY);
+				break;
+			case "Partly Cloudy":
+				icons.set("condition", Skycons.PARTLY_CLOUDY_DAY);
+				break;
+			case "Partly Sunny":
+				icons.set("condition", Skycons.PARTLY_CLOUDY_DAY);
+				break;
+			case "Freezing Rain":
+				icons.set("condition", Skycons.SLEET);
+				break;
+			default:
+				icons.set("condition", Skycons.CLEAR_DAY);
+				break;
+		}
+		icons.play();
 	};
 
 	getGeoLocation();
-});
+}); 
